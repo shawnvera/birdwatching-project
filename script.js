50eda9213a24ef1d5ad2317ca1e30031763d8415
Shawn Vera <shawnvera@gmail.com>
2023-05-25 14:53:43 +0000

    added js for lighten/darken button

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ btn.textContent = 'Darken';
 document.body.appendChild(btn);
 btn.addEventListener('click', () => {
     if (btn.textContent === 'Darken')
-    btn.textContent = "Lighter"
+    darken();
     else
-    btn.textContent = "Darken"
+    lighten();
 })
+
+function changeColor(color) {
+    document.body.style.background = color;
+}
+
+function darken() {
+    btn.textContent = 'Lighter';
+    changeColor('grey');
+}
+
+function lighten() {
+    btn.textContent = 'Darken';
+    changeColor('white');
+}
