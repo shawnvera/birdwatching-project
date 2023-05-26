433b191744dc95cdc86c7c334e4e6393ee9bd311
Shawn Vera <shawnvera@gmail.com>
2023-05-26 01:23:23 +0000

    remove js that was not useful

--- script.js
+++ script.js
@@ -21,7 +21,3 @@ function lighten() {
     btn.textContent = 'Dark';
     changeColor('white');
 }
-
-let vid = createElement('video');
-vid.src ='https://player.vimeo.com/external/377005507.sd.mp4?s=c9f1b7be7875c42bd81d65898d425b75703183ce&profile_id=165&oauth2_token_id=57447761';
-document.body.appendChild(vid);
